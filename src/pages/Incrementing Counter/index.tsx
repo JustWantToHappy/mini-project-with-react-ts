e01983998b44cc9cf819aeced5d8b7fabc0b5519
啦啦啦啦啦 <1302755003@qq.com>
2023-05-23 16:46:16 +0800

    feat Incrementing Counter

---
+++ src/pages/Incrementing Counter/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+type Count = Partial<{
+  twitter: number;
+  youtube: number;
+  facebook: number;
+}>
+
+export default function Index() {
+  const [count, setCount] = React.useState<Count>({});
+
+  React.useEffect(() => {
+    const updater = (counter: "twitter" | "youtube" | "facebook", maxCount: number, currentCount = 0) => {
+      if (currentCount <= maxCount) {
+        setCount(count => {
+          count[counter] = currentCount;
+          return { ...count };
+        });
+        setTimeout(() => {
+          updater(counter, maxCount, currentCount + Math.floor(maxCount / 200));
+        }, 1);
+      }
+    }
+    updater("twitter", 12000);
+    updater("youtube", 5000);
+    updater("facebook", 7500);
+  }, []);
+  const { twitter, facebook, youtube } = count;
+  const container = "flex flex-col justify-center text-center mx-[30px] my-[50px]"
+  return (
+    <main className="h-[100vh] w-full bg-[#8e44ad] flex justify-center items-center overflow-hidden m-0 text-white flex-col sm:flex-row">
+      <div className={container}>
+        <i className="iconfont icon-twitter text-4xl"></i>
+        <div className="text-6xl my-4" data-target="12000">
+          {twitter}
+        </div>
+        <span>Twitter Followers</span>
+      </div>
+      <div className={container}>
+        <i className="iconfont icon-you2 text-4xl"></i>
+        <div className="text-6xl my-4" data-target="5000">
+          {youtube}
+        </div>
+        <span>YouTube Subscribers</span>
+      </div>
+      <div className={container}>
+        <i className="iconfont icon-facebook text-4xl"></i>
+        <div className="text-6xl my-4" data-target="7500">
+          {facebook}
+        </div>
+        <span >Facebook Fans</span>
+      </div>
+    </main>
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,31 +2,6 @@ module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     //如果设置了colors,就只能够使用这里的color样式了。
-    colors: {
-      orange: {
-        light: '#FBBF24',
-        DEFAULT: '#ff6900',
-        dark: '#F59E0B',
-      },
-      gray: {
-        darkest: '#1f2d3d',
-        dark: '#3c4858',
-        DEFAULT: '#c0ccda',
-        light: '#e0e6ed',
-        lightest: '#f9fafc',
-      },
-      border: {
-        DEFAULT: '#d4d4d4',
-      },
-      white: {
-        DEFAULT: '#ffffff',
-      },
-      black: {
-        light: '#404040',
-        DEFAULT: '#262626',
-        dark: '#171717',
-      },
-    },
     extend: {
       zIndex: {
         1: '1',
